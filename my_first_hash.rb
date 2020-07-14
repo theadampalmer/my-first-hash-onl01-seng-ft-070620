1f9ff593941946c5fee1085214bf2bfe599944e4
Adam Palmer <theadampalmer@gmail.com>
2020-07-14 18:43:22 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -13,15 +13,8 @@ def retrieval
 end
 
 def adding
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
-  # add 2 muskets to the shipping_manifest hash below
-  shipping_manifest["muskets"] = 2
-  # add 4 gun powder to the shipping_manifest hash below
-  shipping_manifest["gun powder"] = 4
-  # return the shipping_manifest hash below
-  shipping_manifest
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+    shipping_manifest["muskets"] = 2
+    shipping_manifest["gun powder"] = 4
+    shipping_manifest
 end
